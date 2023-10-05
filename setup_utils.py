371ec272590c3774e41a6de8b43780133b297124
mufeili <mufeili1996@gmail.com>
2023-10-04 23:12:18 -0400

    update

--- setup_utils.py
+++ setup_utils.py
@@ -1,5 +1,61 @@
+import pydantic
 import yaml
+
+from typing import Optional
+
+# pydantic allows checking field types when loading configuration files
+class MetaDataYaml(pydantic.BaseModel):
+    variant: str
+
+class GNNXYaml(pydantic.BaseModel):
+    hidden_t: int
+    hidden_X: int
+    hidden_Y: int
+    num_gnn_layers: int
+    dropout: float
+
+class GNNEYaml(pydantic.BaseModel):
+    hidden_t: int
+    hidden_X: int
+    hidden_Y: int
+    hidden_E: int
+    num_gnn_layers: int
+    dropout: float
+
+class DiffusionYaml(pydantic.BaseModel):
+    T: int
+
+class OptimizerYaml(pydantic.BaseModel):
+    lr: float
+    weight_decay: Optional[float] = 0.
+    amsgrad: Optional[bool] = False
+
+class LRSchedulerYaml(pydantic.BaseModel):
+    factor: float
+    patience: int
+    verbose: bool
+
+class TrainYaml(pydantic.BaseModel):
+    num_epochs: int
+    val_every_epochs: int
+    patient_epochs: int
+    max_grad_norm: Optional[float] = None
+    batch_size: int
+    val_batch_size: int
+
+class SyncYaml(pydantic.BaseModel):
+    meta_data: MetaDataYaml
+    gnn_X: GNNXYaml
+    gnn_E: GNNEYaml
+    diffusion: DiffusionYaml
+    optimizer_X: OptimizerYaml
+    optimizer_E: OptimizerYaml
+    lr_scheduler: LRSchedulerYaml
+    train: TrainYaml
 
 def load_train_yaml(data_name, model_name):
     with open(f"configs/{data_name}/train_{model_name}.yaml") as f:
         yaml_data = yaml.load(f, Loader=yaml.loader.SafeLoader)
+
+    if model_name == "Sync":
+        return SyncYaml(**yaml_data).model_dump()
