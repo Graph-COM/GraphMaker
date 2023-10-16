04d407bf80be4445eb659b76624f9842587952c6
mufeili <mufeili1996@gmail.com>
2023-10-15 23:45:27 -0400

    update

--- eval_utils.py
+++ eval_utils.py
@@ -86,7 +86,51 @@ class Evaluator:
         return dgl_g
 
     def add_mask_citeseer(self, dgl_g, Y_one_hot):
-        pass
+        num_nodes = dgl_g.num_nodes()
+        train_mask = torch.zeros(num_nodes)
+        val_mask = torch.zeros(num_nodes)
+        test_mask = torch.zeros(num_nodes)
+
+        # Based on the raw graph
+        num_val_nodes = {
+            0: 29,
+            1: 86,
+            2: 116,
+            3: 106,
+            4: 94,
+            5: 69
+        }
+
+        num_test_nodes = {
+            0: 77,
+            1: 182,
+            2: 181,
+            3: 231,
+            4: 169,
+            5: 160
+        }
+
+        num_classes = Y_one_hot.size(-1)
+        for y in range(num_classes):
+            nodes_y = (Y_one_hot[:, y] == 1.).nonzero().squeeze(-1)
+            nid_y = torch.randperm(len(nodes_y))
+            nodes_y = nodes_y[nid_y]
+
+            train_mask[nodes_y[:20]] = 1.
+
+            start = 20
+            end = start + num_val_nodes[y]
+            val_mask[nodes_y[start: end]] = 1.
+
+            start = end
+            end = start + num_test_nodes[y]
+            test_mask[nodes_y[start: end]] = 1.
+
+        dgl_g.ndata["train_mask"] = train_mask.bool()
+        dgl_g.ndata["val_mask"] = val_mask.bool()
+        dgl_g.ndata["test_mask"] = test_mask.bool()
+
+        return dgl_g
 
     def add_mask_benchmark(self, dgl_g, Y_one_hot):
         pass
