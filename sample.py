f15914b3fddbd46679cd91f9a76d13de1f54394f
mufeili <mufeili1996@gmail.com>
2023-10-15 22:03:49 -0400

    update

--- sample.py
+++ sample.py
@@ -1,11 +1,24 @@
+import torch
+
 def main(args):
-    pass
+    state_dict = torch.load(args.model_path)
+    dataset = state_dict["dataset"]
+
+    train_yaml_data = state_dict["train_yaml_data"]
+    model_name = train_yaml_data["meta_data"]["variant"]
+
+    print(f"Loaded GraphMaker-{model_name} model trained on {dataset}")
+    print(f"Val Nll {state_dict['best_val_nll']}")
+
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
 
     parser = ArgumentParser()
     parser.add_argument("--model_path", type=str, help="Path to the model.")
+    parser.add_argument("--num_samples", type=int, default=10,
+                        help="Number of samples to generate.")
     args = parser.parse_args()
 
     main(args)
