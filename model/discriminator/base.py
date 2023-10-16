aa5a3c1e9dacfba563cb7415ec52ad34b0865132
mufeili <mufeili1996@gmail.com>
2023-10-16 01:07:25 -0400

    update

--- model/discriminator/base.py
+++ model/discriminator/base.py
@@ -1,4 +1,6 @@
 import itertools
+import numpy as np
+import os
 import torch
 
 class BaseTrainer:
@@ -32,3 +34,69 @@ class BaseTrainer:
             "model_state_dict": self.model.state_dict(),
             "model_config": self.best_model_config
         }, model_path)
+
+class BaseEvaluator:
+    def __init__(self,
+                 Trainer,
+                 model_path,
+                 num_classes,
+                 train_mask,
+                 val_mask,
+                 test_mask,
+                 **real_data):
+        self.Trainer = Trainer
+        self.num_classes = num_classes
+        self.train_mask_real = train_mask
+        self.val_mask_real = val_mask
+        self.test_mask_real = test_mask
+        self.real_data = real_data
+
+        self.model_real = Trainer()
+        if os.path.exists(model_path):
+            self.model_real.load_model(model_path)
+        else:
+            self.model_real.fit(num_classes=num_classes,
+                                train_mask=train_mask,
+                                val_mask=val_mask,
+                                **real_data)
+            self.model_real.save_model(model_path)
+
+        self.real_real_acc = self.model_real.predict(
+            mask=test_mask, **real_data)
+
+        self.real_sample_acc = []
+        self.sample_real_acc = []
+        self.sample_sample_acc = []
+
+    def add_sample(self,
+                   train_mask,
+                   val_mask,
+                   test_mask,
+                   **sample_data):
+        self.real_sample_acc.append(
+            self.model_real.predict(mask=test_mask, **sample_data)
+        )
+
+        model_sample = self.Trainer()
+        model_sample.fit(num_classes=self.num_classes,
+                         train_mask=train_mask,
+                         val_mask=val_mask,
+                         **sample_data)
+
+        self.sample_real_acc.append(
+            model_sample.predict(mask=self.test_mask_real, **self.real_data)
+        )
+
+        self.sample_sample_acc.append(
+            model_sample.predict(mask=test_mask, **sample_data)
+        )
+
+    def summary(self):
+        print(f"ACC/AUC(G|G): {self.real_real_acc}")
+        mean_sample_real_acc = np.mean(self.sample_real_acc)
+        print(f"ACC/AUC(G|G_hat): {mean_sample_real_acc}")
+
+        mean_sample_sample_acc = np.mean(self.sample_sample_acc)
+        print(f"ACC/AUC(G_hat|G_hat): {mean_sample_sample_acc}")
+        mean_real_sample_acc = np.mean(self.real_sample_acc)
+        print(f"ACC/AUC(G_hat|G): {mean_real_sample_acc}")
